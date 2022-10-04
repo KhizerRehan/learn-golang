31a2e65d94f7a8d98982ffc4d93ea08bf7e7b4e7
khizerrehan <khizerrehan92@gmail.com>
2022-10-04 22:47:01 +0500

    add go files

---
+++ practice.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Frontend Masters!!")
+	fmt.Print("Hello Again!!") // Needs to add "\n" to move it next line
+	fmt.Print("Hello Again!!")
+
+	fmt.Printf("Hello, my name is %s. It is September %d", "Khizer", 24)
+}
