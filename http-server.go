07abf538700d8e4d0a14a51b7519613990a9a9bb
khizerrehan <khizerrehan92@gmail.com>
2022-10-26 13:00:16 +0500

    update files

--- http-server.go
+++ http-server.go
@@ -1,25 +1,29 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// )
+import (
+	"fmt"
+	"net/http"
+)
 
-// func hello(w http.ResponseWriter, req *http.Request) {
-// 	fmt.Fprintf(w, "hello\n")
-// }
+func dashboard(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Dashboard")
+}
 
-// func headers(w http.ResponseWriter, req *http.Request) {
-// 	for name, headers := range req.Header {
-// 		for _, h := range headers {
-// 			fmt.Fprintf(w, "%v: %v\n", name, h)
-// 		}
-// 	}
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
-// }
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
-// func main() {
-// 	http.HandleFunc("/hello", hello)
-// 	http.HandleFunc("/headers", headers)
-// 	http.ListenAndServe(":9090", nil)
-// }
+func main() {
+	http.HandleFunc("/dashboard", dashboard)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.ListenAndServe(":9090", nil)
+}
