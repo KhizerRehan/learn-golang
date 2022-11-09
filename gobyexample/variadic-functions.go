082f5f00d17b528bd7bcded20a3980c0fc94bf9e
khizerrehan <khizerrehan92@gmail.com>
2022-11-09 18:37:10 +0500

    add variadic-function example

---
+++ gobyexample/variadic-functions.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+
+	fmt.Printf("Total Sume is %v\n", total)
+}
+
+func main() {
+	fmt.Println("Variadic Function Examples")
+	sum(5, 10)
+	sum(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4, 5}
+	sum(nums...)
+}
